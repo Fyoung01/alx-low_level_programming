f9a5c2befa26cb2c26a16592661da37d49ce6ed8
KolawoleSolomonOgunsakin <kolawolesamson820@gmail.com>
2022-09-11 15:23:38 -0700

    A program that prints all possible combinations of single-digit numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints all possible combinations of single-digit numbers
+ * numbers must be seperated by , , followed by a space
+ * numbers should be printed in ascending order
+ * Return: 0
+ */
+int main(void)
+{
+int n1 = 48;
+int n2;
+int com = 44;
+while (n1 <= 57)
+{
+n2 = n1 + 1;
+while (n2 <= 57)
+{
+putchar(n1);
+putchar(n2);
+if (n1 != 56 || n2 != 57)
+{
+putchar(com);
+putchar(32);
+}
+n2 <= 1;
+}
+n1 <= 1;
+}
+putchar('\n');
+}
