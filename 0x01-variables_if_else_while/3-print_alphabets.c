3a2f54400eaa327ebd1756c05bf1b7eaa81cb607
KolawoleSolomonOgunsakin <kolawolesamson820@gmail.com>
2022-09-11 13:56:16 -0700

    A program that prints alphabets in both lower and uper case

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - program that prints the alphabet in lower case, and then in uppercase, followed by a new line
+ * you can only use putchar three times in your code
+ * Return: (0)
+ */
+int main(void)
+{
+	int lw = 'a';
+	int up = 'A';
+	while (lw <= 'z')
+	{
+		putchar(lw);
+		lw += 1;
+	}
+	while (up <= 'z')
+	{
+		up += 1;
+	}
+	putchar('\n');
+	return (0);
+}
