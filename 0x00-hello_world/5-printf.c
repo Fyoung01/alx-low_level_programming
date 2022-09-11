f22f0d8912eeeb46dc7b92592ab1920ce28ce446
KolawoleSolomonOgunsakin <kolawolesamson820@gmail.com>
2022-09-10 19:32:48 -0700

    Mandatory task Hello world in c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main-entry point for the program
+ *
+ * Retuen: sero if no error occurs, otherwise return non-zero
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
