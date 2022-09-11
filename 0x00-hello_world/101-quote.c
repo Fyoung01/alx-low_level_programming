1d4e6a0731058c8a80811378215faa0d61011e08
KolawoleSolomonOgunsakin <kolawolesamson820@gmail.com>
2022-09-10 20:37:18 -0700

    second advanced task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 /**
- * main-program entry point
+ * main - entry to program
  *
- * Return: 1 on success
+ * Return: 1 success, otherwise, not success
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2,
+			"and that piece of art is useful\" - Dora Korpa, 2015-10-19\n",
+			59);
 	return (1);
 }
-
