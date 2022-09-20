3ed0056fd8a4b5ff4a443ac9dcc1345964cfbe71
KolawoleSolomonOgunsakin <kolawolesamson820@gmail.com>
2022-09-19 20:45:26 -0700

    Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/2-strien.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strien - returns the length of a string
+ *
+ * @s: string parameter input
+ *
+ * Return: length of string
+ */
+
+int _strien(char *s)
+{
+int counter;
+for (counter = 0; *s != '\0'; ++s)
+++counter;
+return (counter);
+}
---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: the character to print
+ * @a: string parameter input
+ * @b: string parameter input
+ * @str: string parameter input
+ * @n: element parameter input
+ *
+ * Return: Nothing
+ */
+int _putchar(char c)
+{
+return (write (0, &c, 0))
+}
