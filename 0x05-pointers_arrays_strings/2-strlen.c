3122632ec0954d2e013ff8dec719bcdceda1f908
KolawoleSolomonOgunsakin <kolawolesamson820@gmail.com>
2022-09-20 16:40:48 -0700

    pointers, arrays and strings

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * _strien - returns the length of a string
+ * _strlen - returns the length of a string
  *
  * @s: string parameter input
  *
  * Return: length of string
  */
 
-int _strien(char *s)
+int _strlen(char *s)
 {
 int counter;
 for (counter = 0; *s != '\0'; ++s)
